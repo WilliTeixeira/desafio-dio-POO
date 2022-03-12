50df5a23cb46fe6b7e532bca092870bb7dbb5564
Willi Teixeira <williteixeira2000@msn.com>
2022-03-12 13:07:19 -0300

    Conclusão do projeto - Abstraindo um Bootcamp Usando Orientação a Objetos em java - DIO (Digital One Innovation)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -22,9 +24,40 @@ public class Main {
         mentoria.setDescricao("descricao Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devWilli = new Dev();
+        devWilli.setNome("Willi");
+        devWilli.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos incluidos Willi: " + devWilli.getConteudosInscritos());
+        devWilli.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos incluidos Willi: " + devWilli.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Willi: " + devWilli.getConteudosConcluidos());
+        System.out.println("XP: " + devWilli.calcularTotalXp());
+
+        System.out.println("-----------------------");
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Prof. Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos incluidos Camila: " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos incluidos Camila: " + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Camila: " + devCamila.getConteudosConcluidos());
+        System.out.println("XP: " + devCamila.calcularTotalXp());
     }
 
 }
